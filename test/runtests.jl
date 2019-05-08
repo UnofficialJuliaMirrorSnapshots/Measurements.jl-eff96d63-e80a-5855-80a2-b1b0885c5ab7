8b161be6cf60547eb0f9c2cc3e7b1b7ef07a3639
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-08 00:57:42 +0000

    Snapshot of branch master taken on 2019-05-07T20:57:41.428-04:00 from "https://github.com/UnofficialJuliaMirror/Measurements.jl-eff96d63-e80a-5855-80a2-b1b0885c5ab7"

--- test/runtests.jl
+++ test/runtests.jl
@@ -437,12 +437,12 @@ end
 end
 
 @testset "Factorial and gamma" begin
-    @test @inferred(SpecialFunctions.factorial(x)) ≈ measurement(6, 0.7536706010590813)
+    @test @inferred(factorial(x)) ≈ measurement(6, 0.7536706010590813)
     @test @inferred(digamma(y)) ≈ 1.256117668431802 ± 0.056764591147422994
     @test @inferred(polygamma(3, w)) ≈ 193.40909103400242 ± 0.10422749480000776
     for a in (w, x, y)
-        @test @inferred(gamma(a)) ≈ SpecialFunctions.factorial(a - one(a))
-        @test @inferred(gamma(a + one(a))) ≈ SpecialFunctions.factorial(a)
+        @test @inferred(gamma(a)) ≈ factorial(a - one(a))
+        @test @inferred(gamma(a + one(a))) ≈ factorial(a)
         @test @inferred(lgamma(abs(a))) ≈ log(gamma(abs(a)))
         @test @inferred(digamma(a)) ≈ polygamma(0, a)
         @test @inferred(digamma(invdigamma(a))) ≈ a + zero(a)
@@ -751,6 +751,10 @@ end
     @test @inferred(1.0u"s" ± 0.1 * u"s") == measurement(1.0, 0.1) * u"s"
     @test @inferred(1.3 * u"km" ± 2.01 * u"μm") == measurement(1300, 2.01e-6) * u"m"
     @test_throws MethodError 1.0u"s" ± 0.1u"m"
+
+    x = 1u"m" ± .2u"m"
+    @test value(x) == 1u"m"
+    @test uncertainty(x) == .2u"m"
 end
 
 @testset "Complex measurements" begin
